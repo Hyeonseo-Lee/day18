d138d3b7ee442ef1e1c784be174daa8e6667e9ab
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-13 14:43:36 +0900

    day02 number2

--- day02.py
+++ day02.py
@@ -1,15 +1,17 @@
 # Chap 3
-print(2*2*2*2*2)
-print(2**5)
-print(pow(2, 5))
-print(divmod(9, 5))
-print(type(divmod(9, 5)))
-print(type((1, 2)))
-test = 1, 2    #packing
-print(type(test))
-print(test)
-print(test[1])
-a, b = test
-# a, b, c = test #unpacking
-print(a)
-print(b)
+
+#number = 0b10011010
+#number = 0x9A
+#number = 0o232
+#print(number) #128+16+8+2=154
+
+number = 154
+print(bin(number))
+print(hex(number))
+print(oct(number))
+
+print(ord(" a ")) # ascii
+print(hex(ord(" "))) #07
+
+
+
