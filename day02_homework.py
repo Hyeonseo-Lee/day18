5a125816138736ad29592720f2becbeb8fb71b96
Hyeonseo-Lee <lhss1054@naver.com>
2023-01-13 15:52:04 +0900

    4-1

---
+++ day02_homework.py
@@ -0,0 +1,13 @@
+import random
+secret = int(random.randint(1, 10)) #1에서 10 사이의 정수가 임의로 발생
+guess = int(random.randint(1, 10))
+print(guess, secret)
+
+if guess < secret:
+    print('too low')
+elif guess > secret:
+    print('too high')
+elif guess == secret:
+    print('just right')
+
+
